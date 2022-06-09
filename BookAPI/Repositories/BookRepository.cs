93a5bec034fe58e6271bb34aba1e2c1461d47181
Mohammed Bello <Mohammed Bello@MOHAMMEDBELLO>
2022-06-09 12:19:41 +0100

    Add project files.

---
+++ BookAPI/Repositories/BookRepository.cs
@@ -0,0 +1,50 @@
+﻿using BookAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookContext _context;
+
+        public BookRepository(BookContext context)
+        {
+            _context = context;
+        }
+        public async Task<Book> Create(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return book;
+           // throw new System.NotImplementedException();
+        }
+
+        public async Task Delete(int id)
+        {
+            var bookToDelete = await _context.Books.FindAsync(id);
+            _context.Books.Remove(bookToDelete);
+            await _context.SaveChangesAsync();
+
+            //throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Book>> Get()
+        {
+            return await _context.Books.ToListAsync();
+           // throw new System.NotImplementedException();
+        }
+
+        public async Task<Book> Get(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        public async Task Update(Book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
